bcb79c66d1e70015016b6fb3aaccc3cbe0078190
0xMcKenna <0xMcKenna@protonmail.com>
2022-01-06 15:29:26 +0000

    Init commit

---
+++ test/assange-test.js
@@ -0,0 +1,13 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Assange", function () {
+
+  beforeEach(async function () {
+    
+  })
+
+  it("", async function () {
+    
+  });
+});
